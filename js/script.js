2e2916ca438bc20e6634c621da28844c8d4d09d8
mahabub <mahabubalom72091@gmail.com>
2023-11-24 21:55:10 +0300

    adding

--- js/script.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function name(params) {
-    var table = $('#example').DataTable({
-        buttons:['copy' , 'csv' , 'excel' , 'pdf' , 'print']
-    });
-
-    table.buttons().container()
-    .appendTo('#example_wrapper .col-md-6:eq(0)');
-    
-});
